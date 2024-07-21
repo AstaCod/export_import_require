a163dd3f77d499dff318744c30e5432b163fc190
AstaCod <actaxandriy@gmail.com>
2024-07-21 12:54:04 -0400

    first commit

---
+++ index_ex_imp.js
@@ -0,0 +1,11 @@
+const upper = require('./strings_imp_exp')
+const {sum,div,extract} = require('./math_imp_exp');
+const mult = require('./mult_imp_exp')
+const console = require("node:console");
+
+console.log('Hello JS 123');
+console.log(sum(5,7));
+console.log(div(5,7))
+console.log(extract(5,7));
+console.log(upper('abc'))
+console.log(mult(5,7));
---
+++ mult_imp_exp.js
@@ -0,0 +1,4 @@
+function mult(n1, n2) {
+    return n1 * n2
+}
+module.exports = mult;
